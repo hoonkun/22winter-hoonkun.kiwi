33edf248997c43af5ffa5eafdbc093652f332442
hoonkun <hoonkun@unstabler.pl>
2022-12-10 15:26:57 +0900

    fix: MaterialIcon 이 span의 일반 prop을 받지 못하던 문제 수정

--- src/components/MaterialIcon.tsx
+++ src/components/MaterialIcon.tsx
@@ -1,9 +1,11 @@
 import React, { CSSProperties } from "react";
 import styled from "@emotion/styled";
 
-const MaterialIcon: React.FC<{ style?: CSSProperties, className?: string, i: string }> = ({ style, className, i }) => {
+type SpanProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+
+const MaterialIcon: React.FC<{ style?: CSSProperties, className?: string, i: string } & SpanProps> = ({ style, className, i, ...props }) => {
   return (
-    <StyledMaterialIcon className={`${className} material-icons`} style={style}>{i}</StyledMaterialIcon>
+    <StyledMaterialIcon {...props} className={`${className} material-icons`} style={style}>{i}</StyledMaterialIcon>
   )
 }
 
