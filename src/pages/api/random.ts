3d6f09db57fb916a052836ade927eb43259478d5
hoonkun <hoonkun@unstabler.pl>
2022-12-11 13:12:28 +0900

    checkpoint: 랜덤 메시지를 추가

--- src/pages/api/random.ts
+++ src/pages/api/random.ts
@@ -4,6 +4,24 @@ import { Arrays } from "../../utils/Array";
 
 
 const Randoms: Data[] = [
+  // { text: "", image: "", duration: 0 }
+  { text: "스타벅스 죽돌이던 때가 있었습니다.\n거의 매일 스타벅스에 앉아있었죠.", image: "starbucks.jpg", duration: 4000 },
+  { text: "쓸만한 신발이 하나 갖고싶습니다.", image: "shoes.jpg", duration: 3000 },
+  { text: "트위터 인생에서 RT를 타본 적이 딱 한 번 있습니다.", image: "rt_star.jpg", duration: 3500 },
+  { text: "이런걸 파던 때도 있었습니다. 지금은 아니지만요.", image: "rhythm.jpg", duration: 4500 },
+  { text: "지금까지 가끔이지만 즐겨하는 리듬겜입니다.\n아무도 안해서 대기가 없는게 쾌적해서 좋아요.", image: "rhythm_2.jpg", duration: 4500 },
+  { text: "고훈 군은 라면을 좋아합니다.\n컵라면도, 봉지라면도 좋아합니다.\n싸고 간편하잖아요!", image: "ramyeon.jpg", duration: 5000 },
+  { text: "지금까지 일본 여행을 세 번 다녀왔는데\n모두 오사카(쿄토)로 다녀왔습니다.\n도쿄에 가보고싶어요.", image: "oosaka_2.jpg", duration: 5500 },
+  { text: "지금까지 일본 여행을 세 번 다녀왔는데\n모두 오사카(쿄토)로 다녀왔습니다.\n도쿄에 가보고싶어요.", image: "oosaka.jpg", duration: 5500 },
+  { text: "마인크래프트에서 광질은 사진처럼 합니다.\n지하에서 하늘을 볼 수 있죠.", image: "minecraft.jpg", duration: 4500 },
+  { text: "일본의 교자 만두는 편의점에서 파는것도 맛있더군요.", image: "gyoja.jpg", duration: 3500 },
+  { text: "학교 다닐 때는 엉뚱한 친구들이 곁에 있었습니다.", image: "friends.jpg", duration: 3500 },
+  { text: "학교 다닐 때는 엉뚱한 친구들이 곁에 있었습니다.", image: "friends_2.jpg", duration: 3500 },
+  { text: "컴공과는 이런거 찍는 직업이라던데요?\n물론 저는 컴공과가 아닙니다.", image: "comgong.jpg", duration: 4500 },
+  { text: "짹. 짹짹.. 짹 짹짹 짹 짹짹짹! 삐약.", image: "chamsae.jpg", duration: 3500 },
+  { text: "불닭을 좋아했습니다. 좋아했었습니다.\n지금도 좋아하고싶습니다.", image: "buldark.jpg", duration: 4500 },
+  { text: "수능이 끝난지 꽤 시간이 지났지만,\n여전히 생명과학 II 문제를 푸는 걸 좋아합니다.", image: "biology_ii.jpg", duration: 5000 },
+  { text: "통학 왕복 4시간이 걸리는\n학교에 다니던 시절이 있었습니다.", image: "amumal.jpg", duration: 4000 },
   { text: "고훈 군은 가끔 떡볶이를 좋아합니다.\n가끔이요.", image: "bbokki.png", duration: 3500 },
   { text: "고훈 군은 마인크래프트를 즐겨 합니다.\n같이할 사람 절찬리에 모집 중!", image: "command_block_front_big.png", duration: 5000 },
   { text: "감자튀김 최고! 너희도 같이 이 감자튀김 먹자!", image: "fries.png", duration: 4000 },
