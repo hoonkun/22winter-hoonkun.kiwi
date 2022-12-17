10bb277ea28199d18e735744c726ce93c0013f8d
hoonkun <hoonkun@unstabler.pl>
2022-12-17 17:35:07 +0900

    fix: 유틸리티 함수들의 타입이 명확하게 설정되도록 함

--- src/utils/KTN.ts
+++ src/utils/KTN.ts
@@ -1,12 +1,14 @@
 declare global {
   interface Array<T> {
     random(): T
-    let<R>(block: (it: T[]) => R): R
-    also(block: (it: T[]) => unknown): T[]
+    get isEmpty(): boolean
+    get lastIndex(): number
   }
   interface Object {
-    let<R>(block: (it: this) => R): R
-    also(block: (it: this) => unknown): this
+    let<T, R>(this: T, block: (it: T) => R): R
+    also<T>(this: T, block: (it: T) => unknown | void): T
+    takeIf<T>(this: T, block: (it: T) => boolean): T | null
+    takeUnless<T>(this: T, block: (it: T) => boolean): T | null
   }
 }
 
@@ -19,9 +21,19 @@ Array.prototype.let = function <R>(block: (it: any) => any) {
 }
 
 Array.prototype.also = function (block: (it: any) => unknown) {
-  block(this);
-  return this;
+  block(this)
+  return this
 }
+
+Object.defineProperty(Array.prototype, "isEmpty", {
+  writable: true, configurable: false, enumerable: false,
+  get: function () { return this.length === 0 }
+})
+
+Object.defineProperty(Array.prototype, "lastIndex", {
+  writable: true, configurable: false, enumerable: false,
+  get: function () { return this.length - 1 }
+})
 
 Object.defineProperty(Object.prototype, "let", {
   value: function <R>(block: (it: any) => R): R { return block(this) },
@@ -30,6 +42,16 @@ Object.defineProperty(Object.prototype, "let", {
 
 Object.defineProperty(Object.prototype, "also", {
   value: function(block: (it: any) => unknown) { block(this); return this; },
+  writable: true, configurable: false, enumerable: false
+})
+
+Object.defineProperty(Object.prototype, "takeIf", {
+  value: function(block: (it: any) => boolean) { return block(this) ? this : null; },
+  writable: true, configurable: false, enumerable: false
+})
+
+Object.defineProperty(Object.prototype, "takeUnless", {
+  value: function(block: (it: any) => boolean) { return block(this) ? null : this; },
   writable: true, configurable: false, enumerable: false
 })
 
