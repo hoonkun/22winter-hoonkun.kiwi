a95ecd3c9eda27bf451dfe481b5846cc6105ba32
hoonkun <herokun.user@gmail.com>
2022-12-28 13:41:56 +0900

    checkpoint: 기존의 포스트 목록을 /posts/[page] 라우트로 옮김

---
+++ src/pages/posts/[page].tsx
@@ -0,0 +1,62 @@
+import { Post, Posts } from "../../utils/Posts";
+import config from "../../config";
+import { ArrayK } from "../../utils/KTN";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import PostsView, { PostPaginator } from "../../components/home/PostsView";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Router from "next/router";
+import { SplashView } from "../../components/SplashView";
+import styled from "@emotion/styled";
+
+export type PostsStaticProps = {
+  posts: Post[]
+  routedPage: number | null
+  total: number
+}
+
+const PostsPage: NextPage<PostsStaticProps> = ({ posts, routedPage: page, total }) => {
+
+  const [loading, setLoading] = useState(false)
+
+  const next = useCallback(() => Router.push(`/posts/${(page ??  1) + 1}`), [page])
+  const previous = useCallback(() => Router.push(`/posts/${(page ?? 1) - 1}`), [page])
+  const navigate = useCallback((page: number) => Router.push(`/posts/${page}`), [])
+
+  const paginator = useMemo<PostPaginator>(() => ({
+    next, previous, navigate, maxPage: (total / config.blog.page_size).ceil, page: page ?? 1
+  }), [next, previous, navigate, total, page])
+
+  useEffect(() => {
+    setLoading(false)
+  }, [posts])
+
+  return (
+    <Root>
+      <PostsView items={posts} paginator={paginator} requestSplash={setLoading}/>
+      <SplashView active={loading}/>
+    </Root>
+  )
+}
+
+export const getStaticProps: GetStaticProps<PostsStaticProps> = context => {
+  const page = parseInt(context.params?.page as string ?? "1");
+  return { props: { posts: Posts.list(page), routedPage: page, total: Posts.total } }
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const pages = Math.ceil(Posts.total / config.blog.page_size)
+
+  return {
+    paths: ArrayK(pages, it => it + 1).map(it => ({ params: { page: `${it}` } })),
+    fallback: false
+  }
+}
+
+const Root = styled.div`
+  background-color: #181818;
+  width: 100%;
+  height: 100%;
+  font-family: "IBM Plex Sans KR", sans-serif;
+`
+
+export default PostsPage
