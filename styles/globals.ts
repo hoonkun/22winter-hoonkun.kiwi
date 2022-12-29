ddab3de8045216947734ca932e1505e7ff37d15a
hoonkun <herokun.user@gmail.com>
2022-12-29 14:10:22 +0900

    fix: 모바일 환경에서 상태바 강제 표시를 위한 height: calc(100% - 1px); 문장이 데스크탑 환경에서는 적용되지 않도록 함

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,12 +1,5 @@
 import { css } from "@emotion/react";
 
-
-export const FullFixed = css`
-  position: fixed;
-  width: 100%;
-  height: calc(100% - 1px);
-`
-
 export const HideScrollbar = css`
   &::-webkit-scrollbar {
     display: none;
@@ -16,6 +9,16 @@ export const HideScrollbar = css`
 export const OverlayOverflow = css`overflow: overlay;`
 
 export const Breakpoint = `@media only screen and (min-width: 840px)`
+
+export const FullFixed = css`
+  position: fixed;
+  width: 100%;
+  height: calc(100% - 1px);
+  
+  ${Breakpoint} {
+    height: 100%;
+  }
+`
 
 export const not = (selector: string) => {
   if (selector === Breakpoint) {
