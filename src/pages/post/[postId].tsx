b20068ab4f36dd7857ce376194717e705706af6a
hoonkun <herokun.user@gmail.com>
2022-12-30 18:28:37 +0900

    fix: 게시글 페이지에서 게시글 목록으로 이동하는 버튼을 추가

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -19,6 +19,7 @@ import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import Highlighter from "react-syntax-highlighter"
 import Head from "next/head";
 import { Breakpoint, ScaleBreakpoint } from "../../../styles/globals";
+import MaterialIcon from "../../components/MaterialIcon";
 
 type PostPageProps = {
   post: Post
@@ -56,6 +57,7 @@ const PostPage: NextPage<PostPageProps> = pageProps => {
     <Root>
       <PostHead post={pageProps.post}/>
       <PostLimitedWidth>
+        <Link href={"/posts/1"}><BackButton i={"arrow_back"}/></Link>
         <PostTitle>{pageProps.post.data.title}</PostTitle>
         <PostDescription>
           <li>{pageProps.post.data.date}</li>
@@ -137,6 +139,17 @@ const PostHead: React.FC<{ post: Post }> = ({ post }) => {
     </Head>
   )
 }
+
+const BackButton = styled(MaterialIcon)`
+  margin-bottom: 20px;
+  margin-top: -5px;
+  margin-left: -5px;
+  padding: 5px;
+  border-radius: 4px;
+  &:hover {
+    background-color: #ffffff30;
+  }
+`
 
 const ExternalLinks = styled.div`
   display: flex;
@@ -278,7 +291,7 @@ const PostTitle = styled.div`
   line-height: 125%;
   
   ${Breakpoint} {
-    margin-top: 40px;
+    margin-top: 20px;
   }
 `
 
