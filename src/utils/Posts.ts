57db6736e3e230c073356c9c3686c73f27b24bff
hoonkun <herokun.user@gmail.com>
2022-12-30 11:26:15 +0900

    feature: 데스크탑 환경에서는 포스트 머리말의 expand 를 사용하여 적절히 그리드 span을 확장할 수 있도록 함

--- src/utils/Posts.ts
+++ src/utils/Posts.ts
@@ -2,22 +2,33 @@ import fs from "fs";
 import matter from 'gray-matter';
 
 import config from "../config";
+import { ArrayK } from "./KTN";
 
-export type PostMetadata = { title: string, date: string, author: string, categories: string[] }
+export type PostMetadata = {
+  title: string
+  date: string
+  author: string
+  categories: string[]
+  expand?: { max_columns: number, max_rows: number }
+}
 
-export type Post = {
+export type RawPost = {
   key: string
   data: PostMetadata
   excerpt: string
 }
+
+export type Post = RawPost & { expand?: { columns: number, rows: number } }
 
 export type PostWithContent = Post & { content: string }
 
 export class Posts {
 
   static get total() {
-    const dir = fs.readdirSync("./_posts")
-    return dir.length
+    return fs
+      .readdirSync("./_posts")
+      .filter(it => !it.startsWith("_"))
+      .length
   }
 
   static list(page?: number): Post[] {
@@ -31,9 +42,56 @@ export class Posts {
         fs.readFileSync(`./_posts/${key}/_post.markdown`, { encoding: "utf8" })
           .let(it => matter(it, { excerpt: true, excerpt_separator: config.blog.excerpt_separator }))
           .pick("data", "excerpt")
-          .also(it => it.data = it.data.pick("title", "date", "author", "categories"))
-          .let(it => ({ ...it, excerpt: it.excerpt?.replace(/^> .+$/gm, ""), key }) as Post)
+          .also(it => it.data = it.data.pick("title", "date", "author", "categories", "expand"))
+          .let(it => ({ ...it, excerpt: it.excerpt?.replace(/^> .+$/gm, ""), key }) as RawPost)
       )
+      .let(it => Posts.withExpand(it))
+  }
+
+  private static withExpand(posts: RawPost[]): Post[] {
+    const GridColumns = 3
+    const GridRows = (1024 / GridColumns).ceil
+    const grid = ArrayK(1024, () => false).chunked(GridColumns)
+    let index = 0
+    const postsMapper = (post: RawPost): Post => {
+      let x = index % GridColumns
+      let y = (index / GridColumns).floor
+
+      while (grid[y][x]) {
+        index++
+        x = index % GridColumns
+        y = (index / GridColumns).floor
+      }
+
+      grid[y][x] = true
+
+      if (index === 0) {
+        grid[y+1][x] = grid[y][x+1] = grid[y+1][x+1] = true
+        return ({ ...post, expand: { columns: 2, rows: 2 } })
+      }
+
+      if (!post.data.expand)
+        return post
+
+      const { max_columns, max_rows } = post.data.expand
+
+      let columns = 1, rows = 1
+
+      while (x + columns <= GridColumns && !grid[y][x + columns] && columns < max_columns) { columns++ }
+      if (x + columns > GridColumns) columns--
+
+      while (y + rows <= GridRows && !grid[y + rows][x + columns] && rows < max_rows) { rows++ }
+      if (y + rows > GridRows) rows--
+
+      for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+          grid[y + j][x + i] = true
+        }
+      }
+
+      return ({ ...post, expand: { columns, rows } })
+    }
+    return posts.map(postsMapper)
   }
 
   static retrieve(key: string): PostWithContent {
